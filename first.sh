d746fb981f3fd69e72f8f4ddb9be23b404f97824
Lam Nguyen <lamnguyen1112@gmail.com>
2022-11-08 23:23:38 +0700

    add examples

---
+++ first.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a comment!
+echo Hello World        # This is a comment, too!
+MESSAGE="hello world"
+echo "$MESSAGE"
+
+echo What is your name ?
+read -r MY_NAME
+echo "Hello $MY_NAME - hope you're well."
